f01dff8a9b67bc25c5c2f196e9ca933c9682e61f
Johnny Chen <johnnyc1314@gmail.com>
2022-05-16 22:39:12 -0400

    commit tree 1a4f2b7ad54161dd1ca85af62785e903a1d18b69

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    def match(list)
+        list.filter do |w|
+            w.split("").sort_by {|c| c} == @word.split("").sort_by {|c| c}
+        end
+    end
+end
+
+word = Anagram.new('word')
+
+p word.match(["test", "drow"])
